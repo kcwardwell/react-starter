272d8fd322f516d321a408181dd57294c61af9be
kcwardwell <kcwardwell@hotmail.com>
2021-02-17 08:43:33 -0500

    Added npm components and modified App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="container">
+  
+      <header className="App-header">
+        <h3>React Starter App</h3>
+        <img src={logo} className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
